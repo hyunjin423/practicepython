9cc6f98c65e16cde299f62851028a0e448ca6351
hyunjin423 <1626476@donga.ac.kr>
2021-12-28 11:58:17 +0900

    rota

---
+++ Rotate90.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def rotate_a_matrix_by_90_degree(a):
+    row_length=len(a)
+    column_length = len(a[0])
+
+    res = [[0]*row_length for _ in range(column_length)]
+    for r in range(row_length):
+        for c in range(column_length):
+            res[c][row_length-1-r]=a[r][c]
+    return res
+
+#로테이션 예시
+a=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+]
+
+print(rotate_a_matrix_by_90_degree(a))
